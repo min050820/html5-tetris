f41af9e412803eeea1e1957aa1b4e0490c2edd13
Yun Yeomin <min050820@gmail.com>
2019-09-07 15:31:59 +0900

    SDL2 추가
    external/ 에는 윈도우용 라이브러리가 들어 있고, Emscripten에서는 그쪽에서 제공하는 버전을 사용합니다.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 // Created by Yun Yeomin <min050820@gmail.com>
 // Licensed under GPLv3
 
-#include <stdio.h>
+#include <cstdio>
+#include <SDL.h>
 
-int main() {
-    printf("Hello, world!\n");
+int main(int argc, char *argv[]) {
+    printf("Hello, SDL2!\n");
+
+    SDL_version ver;
+    SDL_GetVersion(&ver);
+    printf("Version: %d.%d.%d\n", ver.major, ver.minor, ver.patch);
     return 0;
 }
